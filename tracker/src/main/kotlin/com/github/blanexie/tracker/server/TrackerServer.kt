e07a2729d774f9330591fa429540c9d0003e5d0f
xiezc <765150816@qq.com>
2021-09-23 00:20:07 +0800

    deal req

--- tracker/src/main/kotlin/com/github/blanexie/tracker/server/TrackerServer.kt
+++ tracker/src/main/kotlin/com/github/blanexie/tracker/server/TrackerServer.kt
@@ -20,6 +20,8 @@ val log = LoggerFactory.getLogger("announce")!!
 fun Route.announce() {
 
 
+    val peerMap = hashMapOf<String, TrackerReq>()
+
     get("announce") {
 
         val infoHash = call.request.queryParameters["info_hash"]!!
@@ -35,44 +37,66 @@ fun Route.announce() {
         var compact = call.request.queryParameters["compact"]?.toInt()
 
         if (ip == null) {
-            ip = call.request.origin.remoteHost
+            ip = getIpAddress(call.request)
         }
 
-        val req = TrackerReq(infoHash, peerId, port, uploaded, downloaded, left, event, ip, compact, trackerId, numwant)
+
+       val req =  peerMap[infoHash]
+
         val resp = hashMapOf<BeStr, BeObj>()
         resp[BeStr("interval")] = BeInt(3600)
         resp[BeStr("min interval")] = BeInt(30)
         resp[BeStr("incomplete")] = BeInt(0)
         resp[BeStr("complete")] = BeInt(1)
 
+        req?.let {
+            val toByteArray = it.ip!!.split(",").map { it.toByte() }.toByteArray()
+            val ushr = it.port.ushr(4).toByte()
+
+
+        }
 
 
         resp[BeStr("peers")] = BeInt(1)
 
+        peerMap[infoHash] =
+            TrackerReq(infoHash, peerId, port, uploaded, downloaded, left, event, ip, compact, trackerId, numwant)
 
         call.respondText("hello word")
     }
 
 }
 
-val UNKNOWN = "unknown";
+const val UNKNOWN = "unknown";
 
 fun getIpAddress(request: ApplicationRequest): String? {
     val LOCALHOST = "127.0.0.1"
     var ipAddress = request.header("x-forwarded-for")
-    if (ipAddress == null || ipAddress.length == 0 || UNKNOWN != ipAddress) {
+    if (ipAddress == null || ipAddress.isEmpty() || UNKNOWN != ipAddress) {
         ipAddress = request.header("Proxy-Client-IP")
     }
-    if (ipAddress == null || ipAddress.length == 0 || UNKNOWN != ipAddress) {
+    if (ipAddress == null || ipAddress.isEmpty() || UNKNOWN != ipAddress) {
         ipAddress = request.header("WL-Proxy-Client-IP")
     }
-    if (ipAddress == null || ipAddress.length == 0 || UNKNOWN != ipAddress) {
+    if (ipAddress == null || ipAddress.isEmpty() || UNKNOWN != ipAddress) {
+        ipAddress = request.header("HTTP_CLIENT_IP");
+    }
+    if (ipAddress == null || ipAddress.isEmpty() || UNKNOWN != ipAddress) {
+        ipAddress = request.header("HTTP_X_FORWARDED_FOR");
+    }
+    if (ipAddress == null || ipAddress.isEmpty() || UNKNOWN != ipAddress) {
+        ipAddress = request.header("X-Real-IP");
+    }
+
+    if (ipAddress == null || ipAddress.isEmpty() || UNKNOWN != ipAddress) {
         ipAddress = request.origin.remoteHost
         if (LOCALHOST == ipAddress) {
             var inet: InetAddress? = InetAddress.getLocalHost()
             ipAddress = inet!!.hostAddress
         }
     }
+
+
     // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
     // "***.***.***.***".length()
     // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
