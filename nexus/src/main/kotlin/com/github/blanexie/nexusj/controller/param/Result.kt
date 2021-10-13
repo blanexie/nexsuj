d0680be653584475126b1b547f9629738f0a1898
xiezc <765150816@qq.com>
2021-10-13 22:57:43 +0800

    feat:  减少模块

--- nexus/src/main/kotlin/com/github/blanexie/nexusj/controller/param/Result.kt
+++ nexus/src/main/kotlin/com/github/blanexie/nexusj/controller/param/Result.kt
@@ -3,4 +3,4 @@ package com.github.blanexie.nexusj.controller.param
 import com.github.blanexie.dao.UserDO
 
 
-data class Result(val code: Int = 200, val message: String = "", val body: Map<String, Any>? = null)
+data class Result(val code: Int = 20000, val message: String = "", val body: Map<String, Any>? = null)
